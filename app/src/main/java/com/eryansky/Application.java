4ed011a5969c46bb3852956df9b7af78f0bae4e6
eryan <eryanwcp@gmail.com>
2024-02-27 10:04:15 +0800

    适配spring boot 3.2.3

--- app/src/main/java/com/eryansky/Application.java
+++ app/src/main/java/com/eryansky/Application.java
@@ -4,6 +4,8 @@ import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
@@ -30,6 +32,8 @@ import jakarta.servlet.ServletException;
 //                DataSourceAutoConfiguration.class,
 //                DruidDataSourceAutoConfigure.class,
                 DataSourceTransactionManagerAutoConfiguration.class,
+                RedisAutoConfiguration.class,
+                RedisRepositoriesAutoConfiguration.class,
                 LiquibaseAutoConfiguration.class
         })
 public class Application extends SpringBootServletInitializer {
