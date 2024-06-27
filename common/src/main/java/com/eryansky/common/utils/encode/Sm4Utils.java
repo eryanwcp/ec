5bc19f1a6a7648cbc65deca35df40eeb867ef2b7
eryanwcp@gmail.com <eryanwcp@gmail.com>
2024-06-27 21:32:20 +0800

    SM4加密方法

--- common/src/main/java/com/eryansky/common/utils/encode/Sm4Utils.java
+++ common/src/main/java/com/eryansky/common/utils/encode/Sm4Utils.java
@@ -6,14 +6,17 @@ import org.bouncycastle.util.encoders.Hex;
  
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.AlgorithmParameters;
 import java.security.Key;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.util.Arrays;
- 
+
 /**
  * Sm4 国密算法
+ * 支持 ECB和CBC两种模式
  *
  */
 public final class Sm4Utils {
@@ -28,19 +31,34 @@ public final class Sm4Utils {
     // PKCS5Padding-以8个字节为一组进行分组加密
     // 定义分组加密模式使用：PKCS5Padding
     public static final String ALGORITHM_NAME_ECB_PADDING = "SM4/ECB/PKCS5Padding";
+    // 加密算法/分组加密模式/分组填充方式
+    // PKCS5Padding-以8个字节为一组进行分组加密
+    // 定义分组加密模式使用：PKCS5Padding
+    public static final String ALGORITHM_NAME_CBC_PADDING = "SM4/CBC/PKCS5Padding";
+
     // 128-32位16进制；256-64位16进制
     public static final int DEFAULT_KEY_SIZE = 128;
- 
+
     /**
      * 自动生成密钥
      *
      * @return
      * @explain
      */
-    public static String generateKey() throws Exception {
-        return new String(Hex.encode(generateKey(DEFAULT_KEY_SIZE)));
+    public static byte[] generateKey() throws Exception {
+        return generateKey(DEFAULT_KEY_SIZE);
     }
- 
+
+
+    /**
+     * 自动生成密钥
+     * @return
+     * @throws Exception
+     */
+    public static String generateKeyString() throws Exception {
+        return ByteUtils.toHexString(generateKey());
+    }
+
     /**
      * @param keySize
      * @return
@@ -52,7 +70,8 @@ public final class Sm4Utils {
         kg.init(keySize, new SecureRandom());
         return kg.generateKey().getEncoded();
     }
- 
+
+
     /**
      * 生成ECB暗号
      *
@@ -177,24 +196,132 @@ public final class Sm4Utils {
         flag = Arrays.equals(decryptData, srcData);
         return flag;
     }
- 
+
+//    CBC模式
+    /**
+     * sm4加密(CBC)
+     *
+     * @param hexKey   16进制密钥（忽略大小写）
+     * @param paramStr 待加密字符串
+     * @return 返回16进制的加密字符串
+     * @throws Exception
+     * @explain 加密模式：CBC
+     */
+    public static String encrypt(String hexKey, String paramStr) throws Exception {
+        String result = "";
+        // 16进制字符串-->byte[]
+        byte[] keyData = ByteUtils.fromHexString(hexKey);
+        // String-->byte[]
+        byte[] srcData = paramStr.getBytes(ENCODING);
+        // 加密后的数组
+        byte[] cipherArray = encryptCbcPadding(keyData, srcData);
+
+        // byte[]-->hexString
+        result = ByteUtils.toHexString(cipherArray);
+        return result;
+    }
+
+    /**
+     * 加密模式之CBC
+     *
+     * @param key
+     * @param data
+     * @return
+     * @throws Exception
+     * @explain
+     */
+    public static byte[] encryptCbcPadding(byte[] key, byte[] data) throws Exception {
+        Cipher cipher = generateCbcCipher(ALGORITHM_NAME_CBC_PADDING, Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
+
+    /**
+     * 加密模式之CBC
+     * @param algorithmName
+     * @param mode
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    private static Cipher generateCbcCipher(String algorithmName, int mode, byte[] key) throws Exception {
+        Cipher cipher = Cipher.getInstance(algorithmName, BouncyCastleProvider.PROVIDER_NAME);
+        Key sm4Key = new SecretKeySpec(key, ALGORITHM_NAME);
+        cipher.init(mode, sm4Key, generateIV());
+        return cipher;
+    }
+
+    /**
+     * 生成iv
+     * @return
+     * @throws Exception
+     */
+    public static AlgorithmParameters generateIV() throws Exception {
+        //iv 为一个 16 字节的数组，这里采用和 iOS 端一样的构造方法，数据全为0
+        byte[] iv = new byte[16];
+        Arrays.fill(iv, (byte) 0x00);
+        AlgorithmParameters params = AlgorithmParameters.getInstance(ALGORITHM_NAME);
+        params.init(new IvParameterSpec(iv));
+        return params;
+    }
+
+    /**
+     * sm4解密(CBC)
+     *
+     * @param hexKey 16进制密钥
+     * @param text   16进制的加密字符串（忽略大小写）
+     * @return 解密后的字符串
+     * @throws Exception
+     * @explain 解密模式：采用CBC
+     */
+    public static String decrypt(String hexKey, String text) throws Exception {
+        // 用于接收解密后的字符串
+        String result = "";
+        // hexString-->byte[]
+        byte[] keyData = ByteUtils.fromHexString(hexKey);
+        // hexString-->byte[]
+        byte[] resultData = ByteUtils.fromHexString(text);
+        // 解密
+        byte[] srcData = decryptCbcPadding(keyData, resultData);
+        // byte[]-->String
+        result = new String(srcData, ENCODING);
+        return result;
+    }
+
+    /**
+     * 解密
+     *
+     * @param key
+     * @param cipherText
+     * @return
+     * @throws Exception
+     * @explain
+     */
+    public static byte[] decryptCbcPadding(byte[] key, byte[] cipherText) throws Exception {
+        Cipher cipher = generateCbcCipher(ALGORITHM_NAME_CBC_PADDING, Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(cipherText);
+    }
+
+
     public static void main(String[] args) {
         try {
             String paramStr = "Hello, world";
             System.out.println("==========加密前源数据==========");
             System.out.println(paramStr);
             // 生成32位16进制密钥
-            String key = Sm4Utils.generateKey();
+            String key = Sm4Utils.generateKeyString();
             System.out.println("==========生成key==========");
             System.out.println(key);
             String cipher = Sm4Utils.encryptEcb(key, paramStr);
             System.out.println("==========加密串==========");
             System.out.println(cipher);
+            String encrypt = encrypt(key, paramStr);
+            System.out.println(encrypt);
             System.out.println("==========是否为同一数据==========");
             System.out.println(Sm4Utils.verifyEcb(key, cipher, paramStr));
             paramStr = Sm4Utils.decryptEcb(key, cipher);
             System.out.println("==========解密后数据==========");
             System.out.println(paramStr);
+            System.out.println(decrypt(key,encrypt));
         } catch (Exception e) {
             e.printStackTrace();
         }
