308e3b7fe549f3bba1ebe7a2a92149c25ba00e6d
eryanwcp@gmail.com <eryanwcp@gmail.com>
2024-08-31 10:40:40 +0800

    version upgrade

--- app-common/src/main/java/com/eryansky/modules/sys/web/EcServiceController.java
+++ app-common/src/main/java/com/eryansky/modules/sys/web/EcServiceController.java
@@ -19,8 +19,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.*;
 
 /**
