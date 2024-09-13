5dcdce72cbcc6514fe918552de9bd657788aa978
eryanwcp@gmail.com <eryanwcp@gmail.com>
2024-09-13 22:09:54 +0800

    version upgrade

--- j2cache-spring-boot-starter/src/main/java/com/eryansky/j2cache/cache/support/util/J2CacheSerializer.java
+++ j2cache-spring-boot-starter/src/main/java/com/eryansky/j2cache/cache/support/util/J2CacheSerializer.java
@@ -30,6 +30,7 @@ public class J2CacheSerializer implements RedisSerializer<Object>{
 	@Override
 	public Object deserialize(byte[] bytes) throws SerializationException {
 		try {
+			//动态指定序列化方式 ，Fury出现CPU100%，临时强制指定Jackson序列化方式
 //			return SerializationUtils.deserialize(bytes);
 			return jacksonSerializer.deserialize(bytes);
 		} catch (Exception e) {
