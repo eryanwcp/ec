d1b63232f8dfd3cc9c28fbb024300e23bc642de4
eryan <eryanwcp@gmail.com>
2024-10-29 17:58:16 +0800

    spring boot 3.3.5

---
+++ app-common/src/main/java/com/eryansky/core/cms/WordToPdfConverter.java
@@ -0,0 +1,40 @@
+package com.eryansky.core.cms;
+
+import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+
+
+/**
+ * word转pdf 仅支持docx
+ * @author Eryan
+ * @date 2024-10-29
+ */
+public class WordToPdfConverter {
+
+    private static Logger log = LoggerFactory.getLogger(WordToPdfConverter.class);
+
+    /**
+     * word转PDF文件
+     * @param wordFile word文件 仅支持docx格式
+     * @param pdfFile PDF文件
+     * @throws IOException
+     */
+    public void convertDocxToPdf(File wordFile, File pdfFile) throws IOException {
+        try (InputStream in = new FileInputStream(wordFile);
+             OutputStream out = new FileOutputStream(pdfFile)) {
+
+            XWPFDocument document = new XWPFDocument(in);
+            PdfConverter.getInstance().convert(document, out, null);
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+//        convertDocxToPdf(new File("d:\\1.docx"), new File("d:\\2.pdf"));
+    }
+
+}
