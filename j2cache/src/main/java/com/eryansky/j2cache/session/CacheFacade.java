e92b13e7fa1c308c8724c20369dd4258d7d4b229
eryan <eryanwcp@gmail.com>
2024-11-01 10:28:01 +0800

    spring boot 3.3.5

--- j2cache/src/main/java/com/eryansky/j2cache/session/CacheFacade.java
+++ j2cache/src/main/java/com/eryansky/j2cache/session/CacheFacade.java
@@ -212,7 +212,10 @@ public class CacheFacade extends RedisPubSubAdapter<String, String> implements C
         if(this.cache2 == null){
             return;
         }
-
+//        try (StatefulRedisPubSubConnection<String, String> connection = this.pubsub()){
+//            RedisPubSubCommands<String, String> sync = connection.sync();
+//            sync.publish(this.pubsub_channel, cmd.toString());
+//        }
         RedisPubSubCommands<String, String> sync = pubConnection.sync();
         sync.publish(this.pubsub_channel, cmd.toString());
 
