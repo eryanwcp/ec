5166c3c1c8f22b6e524fb13b6fb3c274006bd7a5
eryan <eryanwcp@gmail.com>
2024-12-21 19:22:41 +0800

    1

--- app-common/src/main/java/com/eryansky/core/security/interceptor/AuthorityOauth2Interceptor.java
+++ app-common/src/main/java/com/eryansky/core/security/interceptor/AuthorityOauth2Interceptor.java
@@ -1,8 +1,8 @@
-/**
- * Copyright (c) 2012-2024 https://www.eryansky.com
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- */
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.eryansky.core.security.interceptor;
 
 import com.auth0.jwt.exceptions.TokenExpiredException;
@@ -16,125 +16,112 @@ import com.eryansky.modules.sys._enum.UserType;
 import com.eryansky.modules.sys.mapper.User;
 import com.eryansky.modules.sys.utils.UserUtils;
 import com.google.common.collect.Lists;
+import java.lang.annotation.Annotation;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.lang.annotation.Annotation;
-import java.util.List;
-
-
-/**
- * 模拟Outho2认证拦截器
- * @author Eryan
- * @date 2021-09-09
- */
 public class AuthorityOauth2Interceptor implements AsyncHandlerInterceptor {
-
-
-    protected Logger logger = LoggerFactory.getLogger(getClass());
-
-
-    /**
-     * 不需要拦截的资源
-     */
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
     private List<String> excludeUrls = Lists.newArrayList();
 
-    @Override
+    public AuthorityOauth2Interceptor() {
+    }
+
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //已登录用户
-        SessionInfo sessionInfo = SecurityUtils.getCurrentSessionInfo();
-        if (null != sessionInfo && request.getSession().getId().equals(sessionInfo.getId()) && null != UserType.getByValue(sessionInfo.getUserType())) {
-            return true;
+        String authorization = request.getParameter("Authorization");
+        if (StringUtils.isBlank(authorization)) {
+            authorization = request.getHeader("Authorization");
         }
 
-        //注解处理 满足设置不拦截
-        if(handler instanceof HandlerMethod) {
+        String token = StringUtils.replaceOnce(StringUtils.replaceOnce(authorization, "Bearer ", ""),"Bearer","");
+        SessionInfo sessionInfo = SecurityUtils.getCurrentSessionInfo();
+        if (null != sessionInfo && request.getSession().getId().equals(sessionInfo.getId()) && null != UserType.getByValue(sessionInfo.getUserType())) {
+            if (StringUtils.isBlank(token) || StringUtils.equals(token, sessionInfo.getToken()) || StringUtils.equals(token, sessionInfo.getRefreshToken())) {
+                return true;
+            }
+
+            sessionInfo = null;
+        }
+
+        if (handler instanceof HandlerMethod) {
             HandlerMethod handlerMethod = (HandlerMethod)handler;
-            PrepareOauth2 prepareOauth2Method = handlerMethod.getMethodAnnotation(PrepareOauth2.class);
-            PrepareOauth2 prepareOauth2Class = this.getAnnotation(handlerMethod.getBean().getClass(), PrepareOauth2.class);
-            if ((prepareOauth2Method != null && !prepareOauth2Method.enable()) || (prepareOauth2Class != null && !prepareOauth2Class.enable())) {
+            PrepareOauth2 prepareOauth2Method = (PrepareOauth2)handlerMethod.getMethodAnnotation(PrepareOauth2.class);
+            PrepareOauth2 prepareOauth2Class = (PrepareOauth2)this.getAnnotation(handlerMethod.getBean().getClass(), PrepareOauth2.class);
+            if (prepareOauth2Method != null && !prepareOauth2Method.enable() || prepareOauth2Class != null && !prepareOauth2Class.enable()) {
                 return true;
             }
-            String authType = null != prepareOauth2Method ? prepareOauth2Method.authType():null;
-            if(null == authType){
-                authType = null != prepareOauth2Class ? prepareOauth2Class.authType():null;
+
+            String authType = null != prepareOauth2Method ? prepareOauth2Method.authType() : null;
+            if (null == authType) {
+                authType = null != prepareOauth2Class ? prepareOauth2Class.authType() : null;
             }
-            //非内置用户 自动跳过
-            if(null != authType && !PrepareOauth2.DEFAULT_AUTH_TYPE.equals(authType)){
+
+            if (null != authType && !"user".equals(authType)) {
                 return true;
             }
-            //自动登录
-            String authorization = request.getParameter(AuthorityInterceptor.ATTR_AUTHORIZATION);
-            if (StringUtils.isBlank(authorization)) {
-                authorization = request.getHeader("Authorization");
-            }
+
             if (StringUtils.isNotBlank(authorization)) {
                 String requestUrl = request.getRequestURI();
                 boolean verify = false;
-                String token = StringUtils.replaceOnce(authorization, "Bearer ", "");
-                //判断是否已经登录过
-                if(null == sessionInfo){
+                if (null == sessionInfo) {
                     sessionInfo = SecurityUtils.getSessionInfoByTokenOrRefreshToken(token);
                 }
-                //兼容非内置用户时，自动跳过拦截
-                if(null != sessionInfo && null == UserType.getByValue(sessionInfo.getUserType())){
-//                    logger.warn("{},Token校验失败（用户不存在）,{},{}", sessionInfo.getLoginName(), requestUrl, token);
+
+                if (null != sessionInfo && null == UserType.getByValue(sessionInfo.getUserType())) {
                     return true;
                 }
 
                 String loginName = null;
                 User user = null;
+
                 try {
                     loginName = SecurityUtils.getLoginNameByToken(token);
                     user = UserUtils.getUserByLoginName(loginName);
-                    if(null == user){
-                        logger.warn("Token校验失败（用户不存在）：{},{},{}", loginName, requestUrl, token);
+                    if (null == user) {
+                        this.logger.warn("Token校验失败（用户不存在）：{},{},{}", new Object[]{loginName, requestUrl, token});
                         return true;
                     }
+
                     verify = SecurityUtils.verifySessionInfoToken(token, loginName, user.getPassword());
-                } catch (Exception e) {
-                    if(!(e instanceof TokenExpiredException)){
-                        logger.error("Token校验失败：{},{},{},{},{}",loginName, SpringMVCHolder.getIp(), requestUrl, token, e.getMessage());
+                } catch (Exception var16) {
+                    if (!(var16 instanceof TokenExpiredException)) {
+                        this.logger.error("Token校验失败：{},{},{},{},{}", new Object[]{loginName, SpringMVCHolder.getIp(), requestUrl, token, var16.getMessage()});
                     }
                 }
-                if (verify) {
-                    if(null != sessionInfo){
-                        SecurityUtils.addExtendSession(request.getSession().getId(),sessionInfo.getId());
-                        logger.debug("自动跳过登录：{},{},{},{},{}", loginName, IpUtils.getIpAddr0(request), requestUrl,request.getSession().getId(),sessionInfo.getId());
-                    }else{
-                        SecurityUtils.putUserToSession(request,user);
-                        UserUtils.recordLogin(user.getId());
-                        logger.debug("自动登录成功：{},{},{}", loginName, IpUtils.getIpAddr0(request), requestUrl);
-                    }
 
-                } else {
-//                logger.warn("自动登录失败,{}",authorization);
+                if (verify) {
+                    if (null != sessionInfo) {
+                        SecurityUtils.addExtendSession(request.getSession().getId(), sessionInfo.getId());
+                        this.logger.debug("自动跳过登录：{},{},{},{},{}", new Object[]{loginName, IpUtils.getIpAddr0(request), requestUrl, request.getSession().getId(), sessionInfo.getId()});
+                    } else {
+                        SecurityUtils.putUserToSession(request, user);
+                        UserUtils.recordLogin(user.getId());
+                        this.logger.debug("自动登录成功：{},{},{}", new Object[]{loginName, IpUtils.getIpAddr0(request), requestUrl});
+                    }
                 }
             }
         }
+
         return true;
     }
 
-
-    @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
     }
 
-    @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
         if (e != null) {
-
         }
+
     }
 
-
     public List<String> getExcludeUrls() {
-        return excludeUrls;
+        return this.excludeUrls;
     }
 
     public AuthorityOauth2Interceptor setExcludeUrls(List<String> excludeUrls) {
@@ -151,14 +138,9 @@ public class AuthorityOauth2Interceptor implements AsyncHandlerInterceptor {
         T result = clazz.getAnnotation(annotationType);
         if (result == null) {
             Class<?> superclass = clazz.getSuperclass();
-            if (superclass != null) {
-                return getAnnotation(superclass, annotationType);
-            } else {
-                return null;
-            }
+            return superclass != null ? this.getAnnotation(superclass, annotationType) : null;
         } else {
             return result;
         }
     }
-
 }
