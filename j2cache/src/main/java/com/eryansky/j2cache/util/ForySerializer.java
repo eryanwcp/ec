125728d44ead228b0596ebcf23ac14e85a0a7601
eryan <eryanwcp@gmail.com>
2025-07-04 20:52:29 +0800

    spring boot 3.4.6

--- j2cache/src/main/java/com/eryansky/j2cache/util/ForySerializer.java
+++ j2cache/src/main/java/com/eryansky/j2cache/util/ForySerializer.java
@@ -1,47 +1,47 @@
 package com.eryansky.j2cache.util;
 
-import org.apache.fury.Fury;
-import org.apache.fury.ThreadSafeFury;
-import org.apache.fury.config.Language;
-import org.apache.fury.logging.LoggerFactory;
+import org.apache.fory.Fory;
+import org.apache.fory.ThreadSafeFory;
+import org.apache.fory.config.Language;
+import org.apache.fory.logging.LoggerFactory;
 
 import java.io.IOException;
 
 /**
- * 使用 Apache Fury 实现序列化
- * j2cache.serialization = fury
+ * 使用 Apache Fory 实现序列化
+ * j2cache.serialization = fory
  */
-public class FurySerializer implements Serializer {
+public class ForySerializer implements Serializer {
 
-    private static ThreadSafeFury fury = null;
+    private static ThreadSafeFory fory = null;
 
     static {
         LoggerFactory.useSlf4jLogging(true);
-        fury = Fury.builder().withLanguage(Language.JAVA)
+        fory = Fory.builder().withLanguage(Language.JAVA)
                 // Allow to deserialize objects unknown types, more flexible
                 // but may be insecure if the classes contains malicious code.
                 .withRefTracking(true)
                 .requireClassRegistration(false)
-                .buildThreadSafeFury();
+                .buildThreadSafeFory();
     }
 
-    public FurySerializer(){
+    public ForySerializer(){
 
     }
 
     @Override
     public String name() {
-        return "fury";
+        return "fory";
     }
 
     @Override
     public byte[] serialize(Object obj) throws IOException {
-        return fury.serialize(obj);
+        return fory.serialize(obj);
     }
 
     @Override
     public Object deserialize(byte[] bytes) throws IOException {
-        return fury.deserialize(bytes);
+        return fory.deserialize(bytes);
     }
 
 }
--- j2cache/src/main/java/com/eryansky/j2cache/util/SerializationUtils.java
+++ j2cache/src/main/java/com/eryansky/j2cache/util/SerializationUtils.java
@@ -50,8 +50,8 @@ public class SerializationUtils {
                     g_serializer = Class.forName("com.eryansky.j2cache.util.FstSnappySerializer").asSubclass(Serializer.class).getDeclaredConstructor().newInstance();
                 } else if ("fst-json".equals(ser)) {
                     g_serializer = Class.forName("com.eryansky.j2cache.util.FstJSONSerializer").asSubclass(Serializer.class).getConstructor(Properties.class).newInstance(props);
-                } else if("fury".equals(ser)){
-                    g_serializer = Class.forName("com.eryansky.j2cache.util.FurySerializer").asSubclass(Serializer.class).getDeclaredConstructor().newInstance();
+                } else if("fory".equals(ser) || "fury".equals(ser)){
+                    g_serializer = Class.forName("com.eryansky.j2cache.util.ForySerializer").asSubclass(Serializer.class).getDeclaredConstructor().newInstance();
                 } else if("jackson".equals(ser)){
                     g_serializer = Class.forName("com.eryansky.j2cache.util.JacksonSerializer").asSubclass(Serializer.class).getDeclaredConstructor().newInstance();
                 } else {
