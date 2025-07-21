9a7d8e7ced1dde04d97d485d1241233445ee3141
eryan <eryanwcp@gmail.com>
2025-07-21 15:41:41 +0800

    fory upgrade

--- app-common/src/main/resources/static/static/app/modules/sys/log.js
+++ app-common/src/main/resources/static/static/app/modules/sys/log.js
@@ -50,7 +50,7 @@ $(function () {
             {field: 'extendAttr', title: '自定义参数', width: 200,hidden: true,formatter: function (value, rowData, rowIndex) {
                     delete value['requestData'];
                     var item = JSON.stringify(value);
-                    return item ?? value ?? '';
+                    return item;
             }},
             {field: 'actionTime', title: '操作耗时(ms)', width: 100,align: 'right'},
             {field: 'operTime', title: '操作时间', width: 136, sortable: true}
