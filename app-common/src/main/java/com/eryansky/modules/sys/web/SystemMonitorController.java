43c1a0c29183280dd0259bafbc8b8fa6de06b85b
eryan <eryanwcp@gmail.com>
2025-09-08 10:06:46 +0800

    会话监控

--- app-common/src/main/java/com/eryansky/modules/sys/web/SystemMonitorController.java
+++ app-common/src/main/java/com/eryansky/modules/sys/web/SystemMonitorController.java
@@ -253,7 +253,7 @@ public class SystemMonitorController extends SimpleController {
                 sessionVo.setUpdateTime(Optional.ofNullable(sessionObject).map(v-> Instant.ofEpochMilli(sessionObject.getLastAccess_at()).toDate()).orElse(null));
                 sessionVo.setData(Optional.ofNullable(sessionObject).map(SessionObject::getAttributes).orElse(null));
                 return sessionVo;
-            }).sorted(Comparator.comparing(SessionVo::getUpdateTime).reversed()).collect(Collectors.toList());
+            }).sorted(Comparator.comparing(SessionVo::getUpdateTime).reversed().thenComparing(Comparator.comparing(SessionVo::getUpdateTime).reversed())).collect(Collectors.toList());
             List<SessionVo> dataList = AppUtils.getPagedList(list, page.getPageNo(), page.getPageSize());
             page.autoTotalCount(keys.size());
             page.autoResult(dataList);
