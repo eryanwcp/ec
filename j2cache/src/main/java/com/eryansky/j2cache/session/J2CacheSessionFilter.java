211353ae93f3e8dad6a6692bca42116ef8a62262
eryan <eryanwcp@gmail.com>
2026-02-25 17:43:22 +0800

    version upgrade

--- j2cache/src/main/java/com/eryansky/j2cache/session/J2CacheSessionFilter.java
+++ j2cache/src/main/java/com/eryansky/j2cache/session/J2CacheSessionFilter.java
@@ -238,10 +238,9 @@ public class J2CacheSessionFilter implements Filter {
                             }
                             if(updateCookie){
                                 Cookie cookie = WebUtils.getCookie(request,cookieName);
-                                if(null != cookie){
-                                    WebUtils.deleteCookie(response,cookie,cookiePath);
+                                if(null == cookie){
+                                    setCookie(response,cookieName, finalSession_id);
                                 }
-                                setCookie(response,cookieName, finalSession_id);
                             }
                             return true;
                         }
