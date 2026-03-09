f20a1f0b5aeada911b4d9ea3b332152a96c9a6c4
eryan <eryanwcp@gmail.com>
2026-03-09 14:32:30 +0800

    优化session获取

--- app-common/src/main/java/com/eryansky/core/web/interceptor/LogInterceptor.java
+++ app-common/src/main/java/com/eryansky/core/web/interceptor/LogInterceptor.java
@@ -161,11 +161,12 @@ public class LogInterceptor implements HandlerInterceptor {
 				if (StringUtils.isNotBlank(logging.remark())) {
 					remark = SpringUtils.parseSpel(logging.remark(), handlerMethod.getMethod(), parameterValues);
 				}
-				if (StringUtils.isNotBlank(logging.data())) {
+				//异常情况记录参数
+				if (StringUtils.isNotBlank(logging.data()) || LogType.exception.getValue().equals(logType)) {
 					requestData = SpringUtils.parseSpel(logging.data(), handlerMethod.getMethod(), parameterValues);
 				}
-
-				if (logging.requestHeaders()) {
+				//异常情况记录请求Headers
+				if (logging.requestHeaders() || LogType.exception.getValue().equals(logType)) {
 					requestHeaders= JsonMapper.toJsonString(WebUtils.getHeaders(request));
 				}
 			} else if (logging != null && !Boolean.parseBoolean(SpringUtils.parseSpel(logging.logging(), handlerMethod.getMethod(), parameterValues))) {
