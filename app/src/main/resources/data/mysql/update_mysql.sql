25f88390db0c26199b85e23e5df636dc07491279
eryan <eryanwcp@gmail.com>
2026-04-24 15:55:02 +0800

    优化密码安全配置

--- app/src/main/resources/data/mysql/update_mysql.sql
+++ app/src/main/resources/data/mysql/update_mysql.sql
@@ -17,7 +17,7 @@ ALTER TABLE t_sys_log ADD COLUMN extend_attr text COMMENT '自定义扩展数据
 ALTER TABLE t_sys_log_history ADD COLUMN extend_attr text COMMENT '自定义扩展数据 {\'key1\':Object,\'key2\':Object}';
 
 -- V4.2.20231124.02 消息管理增加消息类型
-ALTER TABLE t_notice_message ADD COLUMN msg_type varchar(32) COMMENT '消息类型 文本：text；文本卡片：textcard';
+ALTER TABLE t_notice_message ADD COLUMN msg_type varchar(36) COMMENT '消息类型 文本：text；文本卡片：textcard';
 
 -- V4.2.20230921.04 用户管理增加“职务”字段
 ALTER TABLE t_sys_user ADD COLUMN position varchar(128) COMMENT '职务';
