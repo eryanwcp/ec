c1c9db6d9b3822906f41ba7eb599a098078a8f2b
eryan <eryanwcp@gmail.com>
2026-06-03 16:23:42 +0800

    add file deletion methods to FileService and update pom.xml version to 4.2.20241221.66

--- app-common/src/main/java/com/eryansky/modules/disk/service/FileService.java
+++ app-common/src/main/java/com/eryansky/modules/disk/service/FileService.java
@@ -220,6 +220,45 @@ public class FileService extends CrudService<FileDao, File> {
         }
     }
 
+
+    /**
+     *
+     * 文件删除 不处理引用
+     * @param fileId 文件ID
+     */
+    public void deleteFileAndClearDisk(String fileId) {
+        File file = dao.get(fileId);
+        try {
+            dao.delete(file);
+            iFileManager.deleteFile(file.getFilePath());
+            logger.debug("删除文件：{}", new Object[]{file.getFilePath()});
+        } catch (IOException e) {
+            logger.error("删除文件[{}]失败,{}", new Object[]{file.getFilePath(), e.getMessage()});
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    /**
+     *
+     * 文件删除 不处理引用
+     * @param file 文件
+     */
+    public void deleteFileAndClearDisk(File file) {
+        if(null == file){
+            return;
+        }
+        try {
+            dao.delete(file);
+            iFileManager.deleteFile(file.getFilePath());
+            logger.debug("删除文件：{}", new Object[]{file.getFilePath()});
+        } catch (IOException e) {
+            logger.error("删除文件[{}]失败,{}", new Object[]{file.getFilePath(), e.getMessage()});
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+
     /**
      *
      * 文件删除（物理删除） 包括文件信息以及仓储文件
