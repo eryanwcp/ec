915168a69df9eafa46cdf795afa41872ea6410da
eryan <eryanwcp@gmail.com>
2026-06-13 10:48:42 +0800

    优化 DBConfigurer 中的 MyBatis 配置逻辑，移除未使用的 mybatisProperties 方法，简化代码结构并提升可读性；更新 pom.xml 中的 sitemesh 版本至 3.2.3

--- app-common/src/main/java/com/eryansky/configure/DBConfigurer.java
+++ app-common/src/main/java/com/eryansky/configure/DBConfigurer.java
@@ -22,10 +22,9 @@ import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.*;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
@@ -62,6 +61,8 @@ public class DBConfigurer {
     @Bean(name = "dataSource")
     @ConfigurationProperties("spring.datasource.druid")
     @Primary
+    @Lazy
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE) // 标记为基础设施 Bean
     public DataSource dataSource(){
         return DruidDataSourceBuilder.create().build();
     }
@@ -76,6 +77,7 @@ public class DBConfigurer {
      * @throws Exception if factory creation fails
      */
     @Bean(name = "sqlSessionFactory")
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public SqlSessionFactory sqlSessionFactoryBean(@Qualifier("dataSource") DataSource dataSource,
                                                    Environment environment) throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
@@ -132,6 +134,7 @@ public class DBConfigurer {
 
     @Order(2)
     @Bean(TX_MANAGER_NAME)
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public TransactionManager annotationDrivenTransactionManager(@Qualifier("dataSource") DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
@@ -142,6 +145,7 @@ public class DBConfigurer {
 
     // 事务的实现Advice
     @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public TransactionInterceptor txAdvice(@Qualifier(TX_MANAGER_NAME) TransactionManager m) {
         NameMatchTransactionAttributeSource source = new NameMatchTransactionAttributeSource();
         RuleBasedTransactionAttribute readOnlyTx = new RuleBasedTransactionAttribute();
@@ -168,6 +172,7 @@ public class DBConfigurer {
     // 切面的定义,pointcut及advice
     @Bean
     @Order(1)
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public Advisor txAdviceAdvisor(@Qualifier("txAdvice") TransactionInterceptor txAdvice,
                                    @Value("${spring.dataSource.aopPointcutExpression}") String aopPointcutExpression) {
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
