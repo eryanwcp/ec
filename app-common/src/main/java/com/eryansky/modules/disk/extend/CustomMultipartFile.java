4691136c5bf8fadf0ddda01dc9dc3d808d974908
eryan <eryanwcp@gmail.com>
2026-06-23 19:45:48 +0800

    优化 CustomMultipartFile.java，重构构造函数并提高性能

--- app-common/src/main/java/com/eryansky/modules/disk/extend/CustomMultipartFile.java
+++ app-common/src/main/java/com/eryansky/modules/disk/extend/CustomMultipartFile.java
@@ -1,44 +1,49 @@
 package com.eryansky.modules.disk.extend;
 
 import org.apache.commons.io.FileUtils;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.activation.MimetypesFileTypeMap;
 import java.io.*;
-import java.nio.file.Files;
+import java.util.Objects;
 
 /**
  * 自定义上传文件对象
+ * 用于将内存字节流模拟为 MultipartFile 接口
+ * @author Eryan
  */
 public class CustomMultipartFile implements MultipartFile, Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 默认附件参数名
+     */
+    private static final String DEFAULT_ATTACHMENT_NAME = "file";
     /**
      * 文件名称
      */
-    private String fileName;
+    private final String fileName;
 
     /**
-     * 文件服务器只接受file参数
+     * 附件参数名
      */
-    private String attachmentName = "file";
+    private final String attachmentName;
 
+    private final byte[] fileData;
 
-    private byte[] fileData;
-
-    public CustomMultipartFile(String fileName,
-                               byte[] fileData) {
-        this.fileName = fileName;
-        this.fileData = fileData;
+    // 缓存 MimeType 类型映射表，避免重复实例化
+    private static final MimetypesFileTypeMap MIME_TYPE_MAP = new MimetypesFileTypeMap();
+    public CustomMultipartFile(String fileName, byte[] fileData) {
+        this(DEFAULT_ATTACHMENT_NAME, fileName, fileData);
     }
 
-    public CustomMultipartFile(String attachmentName, String fileName,
-                               byte[] fileData) {
-        this.fileName = fileName;
-        this.fileData = fileData;
-        this.attachmentName = attachmentName;
+    public CustomMultipartFile(String attachmentName, String fileName, byte[] fileData) {
+        this.attachmentName = Objects.requireNonNull(attachmentName, "attachmentName cannot be null");
+        this.fileName = Objects.requireNonNull(fileName, "fileName cannot be null");
+        this.fileData = Objects.requireNonNull(fileData, "fileData cannot be null");
     }
 
+    @Override
     public String getName() {
         return attachmentName;
     }
@@ -50,7 +55,8 @@ public class CustomMultipartFile implements MultipartFile, Serializable {
 
     @Override
     public String getContentType() {
-        return new MimetypesFileTypeMap().getContentType(fileName);
+        // 使用静态缓存的映射表提高性能
+        return MIME_TYPE_MAP.getContentType(fileName);
     }
 
     @Override
@@ -60,7 +66,7 @@ public class CustomMultipartFile implements MultipartFile, Serializable {
 
     @Override
     public long getSize() {
-        return fileData.length;
+        return fileData != null ? fileData.length : 0L;
     }
 
     @Override
@@ -69,15 +75,16 @@ public class CustomMultipartFile implements MultipartFile, Serializable {
     }
 
     @Override
-    public InputStream getInputStream() {
+    public InputStream getInputStream() throws IOException {
+        if (fileData == null) {
+            throw new IOException("File data is null");
+        }
         return new ByteArrayInputStream(fileData);
     }
 
     @Override
     public void transferTo(File dest) throws IOException {
+        // 直接使用 Commons IO 的高效方法，移除冗余且错误的判断逻辑
         FileUtils.writeByteArrayToFile(dest, fileData);
-        if (dest.isAbsolute() && !dest.exists()) {
-            FileCopyUtils.copy(this.getInputStream(), Files.newOutputStream(dest.toPath()));
-        }
     }
 }
