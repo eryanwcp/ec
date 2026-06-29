b0c3f2bc0f3fa4de70af556ff0a6d3270e826fd3
eryanwcp@gmail.com <eryanwcp@gmail.com>
2026-06-29 23:07:51 +0800

    session 缓存性能优化

--- j2cache/src/main/java/com/eryansky/j2cache/util/ForySerializer.java
+++ j2cache/src/main/java/com/eryansky/j2cache/util/ForySerializer.java
@@ -303,7 +303,6 @@ public class ForySerializer implements Serializer {
                 .withRefTracking(true)
                 .requireClassRegistration(false)
                 .withTypeChecker(typeChecker)
-                .withClassLoader(Thread.currentThread().getContextClassLoader())
                 .buildThreadSafeFory();
     }
 
