0d5713ea77dffdb394167c1509aa349ffcfe2d04
eryan <eryanwcp@gmail.com>
2026-07-10 13:58:38 +0800

    4.2.20241221.70

--- j2cache/src/main/java/com/eryansky/j2cache/util/ForySerializer.java
+++ j2cache/src/main/java/com/eryansky/j2cache/util/ForySerializer.java
@@ -7,9 +7,6 @@ import org.apache.fory.logging.LoggerFactory;
 import org.apache.fory.resolver.AllowListChecker;
 import org.apache.fory.resolver.DisallowedList;
 import org.slf4j.Logger;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.servlet.FlashMap;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Set;
