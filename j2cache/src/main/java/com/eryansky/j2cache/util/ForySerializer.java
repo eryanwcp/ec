a177fb80a7ae02cc4fb19e5b1d6713cc747a418d
eryan <eryanwcp@gmail.com>
2026-07-10 14:00:56 +0800

    添加JsonSerializer类以支持JSON序列化，并优化相关代码

--- j2cache/src/main/java/com/eryansky/j2cache/util/ForySerializer.java
+++ j2cache/src/main/java/com/eryansky/j2cache/util/ForySerializer.java
@@ -7,8 +7,6 @@ import org.apache.fory.logging.LoggerFactory;
 import org.apache.fory.resolver.AllowListChecker;
 import org.apache.fory.resolver.DisallowedList;
 import org.slf4j.Logger;
-import org.springframework.web.servlet.FlashMap;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Set;
@@ -53,7 +51,6 @@ public class ForySerializer implements Serializer {
                 .requireClassRegistration(false)
                 .withTypeChecker(typeChecker)
                 .buildThreadSafeFory();
-        fory.register(FlashMap.class);
     }
 
     public ForySerializer(){
