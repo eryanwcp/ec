e0a43f5b1b06ca3a29c39f1ab0875c184d440fe2
eryan <eryanwcp@gmail.com>
2026-08-16 19:01:11 +0800

    refactor(SSO): rename server URL to login URL for clarity and update references

--- app-common/src/main/java/com/eryansky/utils/AppConstants.java
+++ app-common/src/main/java/com/eryansky/utils/AppConstants.java
@@ -12,7 +12,6 @@ import com.eryansky.common.utils.SysConstants;
 import com.eryansky.common.utils.io.FileUtils;
 import com.eryansky.common.utils.io.PropertiesLoader;
 import com.eryansky.core.rpc.utils.SerializerFactory;
-import com.eryansky.j2cache.util.SerializationUtils;
 import com.eryansky.modules.sys.service.ConfigService;
 
 import java.io.File;
@@ -903,6 +902,15 @@ public class AppConstants extends SysConstants {
     }
 
     /**
+     * SSO单点登录 客户端标识
+     * @return
+     */
+    public static String getSSOClientId() {
+        String code = "system.sso.clientId";
+        return getAppConfig(code, "");
+    }
+
+    /**
      * SSO单点登录页面
      * @return
      */
@@ -927,6 +935,16 @@ public class AppConstants extends SysConstants {
      */
     public static String getSSOIssUser() {
         String code = "system.sso.issuer";
+        return getAppConfig(code, "");
+    }
+
+
+    /**
+     * SSO单点登录 回调地址
+     * @return
+     */
+    public static String getSSOCallbackUrl() {
+        String code = "system.sso.callbackUrl";
         return getAppConfig(code, "");
     }
 
