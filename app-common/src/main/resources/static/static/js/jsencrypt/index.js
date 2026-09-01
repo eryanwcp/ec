86e414fa9d7027846f656cfbc4096c5b9175da41
eryan <eryanwcp@gmail.com>
2026-09-01 11:19:09 +0800

    chore(common): update jsencrypt minified library
    - Update the minified version of the jsencrypt library in
      app-common/src/main/resources/static/static/js/jsencrypt/
      encrypt.all.min.js to ensure the latest bundled version is used.

--- app-common/src/main/resources/static/static/js/jsencrypt/index.js
+++ app-common/src/main/resources/static/static/js/jsencrypt/index.js
@@ -1,10 +1,11 @@
 // esbuild index.js --bundle --minify --outfile=encrypt.all.min.js
 import CryptoJS from './crypto-js.min.js';
 import JSEncrypt from './jsencrypt.min.js';
+import sm4 from './sm4.js';
 import './base64.js';
-import './sm4.js';
 import './encrypt.min.js';
 
 // 显式挂载到全局 window 对象
 window.CryptoJS = CryptoJS;
 window.JSEncrypt = JSEncrypt;
+window.sm4 = sm4;
