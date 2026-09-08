fdce3aa76a66cf367c9f5b20e6af9e8f3d9ea295
eryan <eryanwcp@gmail.com>
2026-09-08 08:46:14 +0800

    refactor(common): optimize data processing and update dependencies
    - Update parent project version to 4.2.20260624.13 in pom.xml.
    - Refactor several utility methods in NoticeService, MessageUtils, RoleUtils, and UserUtils to use Java 8 Stream API instead of custom ConvertUtils/Collections3 for string joining.
    - Improve encryption logic in ExceptionInterceptor to encrypt the 'obj' field and enhance error logging.
    - Remove unused beanToMap method and related imports from ExcelUtils.
    - Add title field to Message inner class in MessageReceiveSimpleVo.
    - Update copyright year to 2026 in upload exception classes.
    - Add new Identifiable interface to j2cache session module.

--- common/src/main/java/com/eryansky/common/utils/collections/Collections3.java
+++ common/src/main/java/com/eryansky/common/utils/collections/Collections3.java
@@ -7,8 +7,11 @@ package com.eryansky.common.utils.collections;
 
 import java.util.*;
 import java.util.function.Predicate;
+
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import com.eryansky.common.utils.reflection.ReflectionUtils;
 
 /**
  * Collections工具集.
@@ -18,6 +21,84 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class Collections3 {
 
+	/**
+	 * 提取集合中的对象的两个属性(通过Getter函数), 组合成Map.
+	 *
+	 * @param collection 来源集合.
+	 * @param keyPropertyName 要提取为Map中的Key值的属性名.
+	 * @param valuePropertyName 要提取为Map中的Value值的属性名.
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+    public static Map extractToMap(final Collection collection, final String keyPropertyName,
+			final String valuePropertyName) {
+		Map map = new HashMap(collection.size());
+
+		try {
+			for (Object obj : collection) {
+				map.put(PropertyUtils.getProperty(obj, keyPropertyName),
+						PropertyUtils.getProperty(obj, valuePropertyName));
+			}
+		} catch (Exception e) {
+			throw ReflectionUtils.convertReflectionExceptionToUnchecked(e);
+		}
+
+		return map;
+	}
+
+	/**
+	 * 提取集合中的对象的一个属性(通过Getter函数), 组合成List.
+	 *
+	 * @param collection 来源集合.
+	 * @param propertyName 要提取的属性名.
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+    public static List extractToList(final Collection collection, final String propertyName) {
+		List list = new ArrayList(collection.size());
+
+		try {
+			for (Object obj : collection) {
+				list.add(PropertyUtils.getProperty(obj, propertyName));
+			}
+		} catch (Exception e) {
+			throw ReflectionUtils.convertReflectionExceptionToUnchecked(e);
+		}
+
+		return list;
+	}
+
+	/**
+	 * 提取集合中的对象的一个属性(通过Getter函数), 组合成Set.
+	 *
+	 * @param collection 来源集合.
+	 * @param propertyName 要提取的属性名.
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static Set extractToSet(final Collection collection, final String propertyName) {
+		Set set = new HashSet(collection.size());
+
+		try {
+			for (Object obj : collection) {
+				set.add(PropertyUtils.getProperty(obj, propertyName));
+			}
+		} catch (Exception e) {
+			throw ReflectionUtils.convertReflectionExceptionToUnchecked(e);
+		}
+
+		return set;
+	}
+
+	/**
+	 * 提取集合中的对象的一个属性(通过Getter函数), 组合成由分割符分隔的字符串.
+	 *
+	 * @param collection 来源集合.
+	 * @param propertyName 要提取的属性名.
+	 * @param separator 分隔符.
+	 */
+	@SuppressWarnings("rawtypes")
+    public static String extractToString(final Collection collection, final String propertyName, final String separator) {
+		List list = extractToList(collection, propertyName);
+		return StringUtils.join(list, separator);
+	}
 
 	/**
 	 * 转换Collection所有元素(通过toString())为String, 中间以 separator分隔。
