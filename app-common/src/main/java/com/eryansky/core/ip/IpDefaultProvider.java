13fde92328fe1a7806124301a68217c50c023982
eryan <eryanwcp@gmail.com>
2026-09-20 13:56:54 +0800

    feat(ip): implement IpAPI interface and default provider for IP location services

--- app-common/src/main/java/com/eryansky/core/ip/IpDefaultProvider.java
+++ app-common/src/main/java/com/eryansky/core/ip/IpDefaultProvider.java
@@ -6,13 +6,19 @@ import com.eryansky.common.utils.net.IpUtils;
 import com.eryansky.core.ip.dto.GeoIP;
 import com.eryansky.utils.AppConstants;
 import com.eryansky.utils.CacheConstants;
+import com.google.common.collect.Maps;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.cache.annotation.Cacheable;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
 public class IpDefaultProvider implements IpAPI {
 
     private static final Logger log = Logger.getLogger(IpDefaultProvider.class);
+
+//    private static final String BASE_API_URL = "https://api.ip.sb";
     private static final String BASE_API_URL = "https://api.seeip.org";
 
 
@@ -31,7 +37,9 @@ public class IpDefaultProvider implements IpAPI {
 
         if(AppConstants.isGeoIpEnable()){
             try {
-                String response = HttpCompoents.getInstance().get(BASE_API_URL + "/geoip/" + ip);
+                Map<String,String> headers = Maps.newHashMap();
+                headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/153.0.0.0 Safari/537.36");
+                String response = HttpCompoents.getInstance().get(BASE_API_URL + "/geoip/" + ip,headers, StandardCharsets.UTF_8.name());
                 return JsonMapper.getInstance().fromJson(response, GeoIP.class);
             } catch (Exception e) {
                 log.error(e.getMessage(), e);
